1184fed11ae5cbac6e5a65626c2df499d1545c83
naman995 <rajputnaman823@gmail.com>
2024-03-21 01:36:32 +0530

    Basic setup with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './pages/website/search/search.component';
+import { BookFlightComponent } from './pages/website/book-flight/book-flight.component';
+import { MyBookingsComponent } from './pages/website/my-bookings/my-bookings.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { AirportComponent } from './pages/admin/airport/airport.component';
+import { AllFlightsComponent } from './pages/admin/all-flights/all-flights.component';
+import { CityComponent } from './pages/admin/city/city.component';
+import { BookingsComponent } from './pages/admin/bookings/bookings.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { NewFlightComponent } from './pages/admin/new-flight/new-flight.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent , title: 'Search Flights'},
+  { path: 'book-flight', component: BookFlightComponent, title: 'Book New Ticket' },
+  { path: 'Bookings', component: MyBookingsComponent, title: 'My Bookings' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'airport', component: AirportComponent, title: 'Airport' },
+      { path: 'city', component: CityComponent, title: 'City' },
+      { path: 'all-bookings', component: BookingsComponent, title: 'Bookings' },
+      {path: 'new-flight', component:NewFlightComponent, title: 'New Flight'}
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { AirportComponent } from './pages/admin/airport/airport.component';
+import { AllFlightsComponent } from './pages/admin/all-flights/all-flights.component';
+import { BookingsComponent } from './pages/admin/bookings/bookings.component';
+import { CityComponent } from './pages/admin/city/city.component';
+import { NewFlightComponent } from './pages/admin/new-flight/new-flight.component';
+import { SearchComponent } from './pages/website/search/search.component';
+import { BookFlightComponent } from './pages/website/book-flight/book-flight.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MyBookingsComponent } from './pages/website/my-bookings/my-bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    AirportComponent,
+    AllFlightsComponent,
+    BookingsComponent,
+    CityComponent,
+    NewFlightComponent,
+    SearchComponent,
+    BookFlightComponent,
+    MyBookingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
